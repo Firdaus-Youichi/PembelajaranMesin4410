84d6795ad38d2acad1c872733e9241fbc3279367
Firdaus Youichi <113011077+Firdaus-Youichi@users.noreply.github.com>
2024-03-25 16:23:13 +0700

    Add files via upload

---
+++ Python-Basic/PB-1.py
@@ -0,0 +1,105 @@
+# Python Basic-1
+"""
+- mengenal fungsi print();
+- tanda kurung buka;
+- tanda kutip;
+- kalimat 1 baris: Hello, World!;
+- fungsi beberapa tanda kutip lain;
+- tanda kurung tutup.
+"""
+
+#1
+print("Hello World@#!#!$")
+print("test")
+
+#2
+print("Hello, Python!")
+print("Firdaus")
+print('Firdaus')
+
+#3
+print("Firdaus\n Youichi")
+print("Firdaus\t Youichi")
+print("Firdaus\r Youichi")
+
+#5
+print("Nama saya \"Youichi\"")
+
+#6
+print("\"")
+
+#7
+print("satu.","dua.","tiga.")
+
+#8
+print("Nama saya","Youichi", end=" ")
+
+#9
+print("satu","dua","tiga", sep=", ")
+
+#10
+print("Programming","Essentials","in")
+print("Python")
+
+#11
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python"*2)
+
+#12
+print("    *    "*2)
+print("   * *   "*2)
+print("  *   *  "*2)
+print(" *     * "*2)
+print("***   ***"*2)
+print("  *   *  "*2)
+print("  *   *  "*2)
+print("  *****  "*2)
+
+#13
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****\n"*2)
+
+#14
+print("2"*2)
+print(2*2)
+
+print(type("2"))
+print(type(2))
+
+#15
+print(5, "mempunyai tipe", type(5))
+print(.5, "mempunyai tipe", type(.5))
+
+#16
+print(True)
+print(False)
+
+print(4>2)
+print(3>4)
+
+#17
+print()
+
+#18
+print('"I\'m"\n""learning""\n"""Python"""')
+
+print("""
+"I'm"
+""learning""
+\"""Python""\"
+""")
+
+#19
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3.)
+print(2. ** 3)
+
+#20
+print(10 / 3)
+print(10 // 3)
+
+print(6 / 3.)
+print(6 // 3.)
+
+print(-4 + 4)
+print(-4. + 8)
